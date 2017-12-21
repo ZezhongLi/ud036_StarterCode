cf4129c97d4a25d0648a396de9d42d9f29a308f0
Zezhong Li <neillizezhong@mail.com>
2017-12-21 14:01:05 -0800

    movie data to data.txt to avoid coding style problem that fails to review

--- media.py
+++ media.py
@@ -1,5 +1,8 @@
 class Movie():
-    """The movie class"""
+    """
+    The movie class assemble data needed for trailer theater
+    for easier calling in the future.
+    """
     def __init__(self, title, storyline, poster_url, trailer_url):
         self.title = title
         self.storyline = storyline
